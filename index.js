5b4d8f21c5a5c9a513af8832f624d5dcd1c15695
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-21 17:07:53 +0300

    Modified indexx.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = `IAN + is the champion`;
+
+document.body.appendChild(newHeader);
